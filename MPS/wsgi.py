7ac1659186fb7e8254d2fa848aa994d6ecafa214
筒井渓祐 <tsutsui@Tsutsui.local>
2021-01-31 13:13:34 +0900

    1/31

--- MPS/wsgi.py
+++ MPS/wsgi.py
@@ -1,18 +1,4 @@
-"""
-WSGI config for MPS project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
-import os
-
-#from dj_static import Cling
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MPS.settings')
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-#application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
